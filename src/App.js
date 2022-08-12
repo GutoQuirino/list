7ed4391764917af51d9100081b92b223df703fdc
GutoQuirino <tavinho_vh@hotmail.com>
2022-08-12 09:37:08 -0300

    recomeço

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import './App.css';
-import Imput from './components/input'
-import Botao from './components/Botao'
-import { useState} from 'react'
-import { useEffect } from 'react';
+
 
 function App() {
 
-  const [lista,setLista]= useState([])
-
-  useEffect(()=>{
-
-    setLista([{tarefa:'Comprar pao'},
-              {tarefa:'Limpar a casa'},
-              {tarefa:'Lavar o banheiro'}
-  ])
-
-  }, [])
-
-  
 
   return (
     <div className="App">
       <h1>LISTA DE TAREFAS</h1>
-      <Imput />
-      <Botao />
-      <hr/>
-      {lista.map((c)=>{
-        return (
-          <ul>
-            <li>{c.tarefa}</li>
-          </ul>
-        )
-      })}
+      
     </div>
   );
 }
--- src/components/Botao.js
+++
@@ -1,9 +0,0 @@
-import '../App.css'
-
-function Botao(){
-    return (
-        <button>Gravar</button>
-    )
-}
-
-export default Botao;
